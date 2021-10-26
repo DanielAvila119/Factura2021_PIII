a24c8183600d0f0027f32c596458475d3e9436bf
DanielAvila119 <90355627+DanielAvila119@users.noreply.github.com>
2021-10-25 19:28:57 -0600

    FACTURA

--- Factura_2021/Factura_2021/Modelos/DAO/Conexión.cs
+++ Factura_2021/Factura_2021/Modelos/DAO/Conexión.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace Factura_2021.Modelos.DAO
 {
     public class Conexión
     {
-
+        protected SqlConnection MiConexion = new SqlConnection(ConfigurationManager.ConnectionStrings["FacturaConexion"].ConnectionString);
     }
 }
